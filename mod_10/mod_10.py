e2af65eb869488257df30682a68e97691eba4de9
shashkow <shashkow@gmail.com>
2024-08-20 09:38:21 +0300

    4 блока из модуля 11 регулярные выражения

--- mod_10/mod_10.py
+++ mod_10/mod_10.py
@@ -1081,3 +1081,42 @@ from collections import namedtuple
 # numbers = list(range(10, 21)) + [30] + list(range(35, 101)) + list(range(1000, 1001))
 
 # print(ranges(numbers))
+
+"""Блок 11.1 Регулярные выражения. Часть 1"""
+
+
+frm6_1 = r'7(-\d{3}){2}(-\d{2}){2}'
+frm6_2 = r'8(-\d{3})(-\d{4}){2}'
+
+regrex_21 = r'beegeek'
+regrex_22 = r'...\....'
+regrex_23 = r'1\d\d'
+regrex_24 = r'\d{3}-\d{3}-\d{4}'
+
+"""Блок 11.2 Регулярные выражения. Часть 2"""
+
+regrex_16 = r'[a-zA-z]\d{4}'
+regrex_17 = r'[a-z]\d[a-z][A-Z]{2}'
+regrex_18 = r'\d[aeiouy][^b^c^D^F]\S[^A^E^I^O^U^Y][^;]'
+regrex_19 = r'[123][012][12x][03aA][xsu][.,]'
+regrex_20 = r'+7'   # ?????
+regrex_22 = r''  #??????
+
+"""Блок 11.3 Регулярные выражения. Часть 3"""
+
+regrex_17 = r'[A-Z]{5}\d{4}[A-Z]'
+regrex_18 = r'<!--*?-->'
+regrex_19 = r'[A-Z]{,3}\d{2,8}[A-Z]{3}'
+regrex_20 = r'[A-Z]{1,2}\d[\d|A-Z]'
+
+"""Блок 11.4 Регулярные выражения. Часть 4"""
+
+regrex_10 = r'[aA]n?'
+regrex_11 = r'\W[A-Z]\W'
+regrex_12 = r'\W[A-Z]*?\W'
+regrex_13 = r'\n*(*)*\n'
+regrex_14 = r'\n\d{2,}[a-z]*[A-Z]*'
+regrex_15 = r'\n[A-Za-z]s\n'
+regrex_16 = r'[(A-Za-z)|(24680)]{40}[(13579)|\s]'
+regrex_17 = r'\n((Mr.)|(Mrs.)|(Ms.)|(Dr.)|(Er.))[A-Za-z]{1,}'
+regrex_18 = r'\n\d{1,2}[A-Za-z]{3,}[.]{1,3}'
