ecdec909328a30d2b6fc038b429b2e16555c4726
shashkow <shashkow@gmail.com>
2024-09-02 22:08:53 +0300

    исправил решение задачи №2

--- course_OOP/mod_2/mod_2.py
+++ course_OOP/mod_2/mod_2.py
@@ -1,4 +1,4 @@
-'''№ 2'''
+'''№ 1 Дартс'''
 
 # with open('input.txt', encoding='utf-8') as input_file:
 #     n = int(input_file.read().strip())
@@ -19,3 +19,20 @@
 
 # for i in range(n):
 #     print(*rez[i])
+
+'''№ 2 Скобочная последовательность'''
+
+# with open('input.txt', encoding='utf-8') as input_file:
+#     inp_str = input_file.read().strip()
+
+# while inp_str.find(')') != -1:
+#     close_parent = inp_str.find(')')
+#     inp_str = inp_str[:close_parent] + inp_str[close_parent + 1:]
+#     if (open_parent := inp_str[:close_parent].rfind('(')) != -1:
+#         inp_str = inp_str[:open_parent] + inp_str[open_parent + 1:]
+
+
+# if inp_str:
+#     print(False)
+# else:
+#     print(True)
