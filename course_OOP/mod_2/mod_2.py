8562e654f0b4a7e2397f825da133921749a0b45e
shashkow <shashkow@gmail.com>
2024-09-05 10:42:44 +0300

    Задачи блок 4,7

--- course_OOP/mod_2/mod_2.py
+++ course_OOP/mod_2/mod_2.py
@@ -58,3 +58,107 @@
 #     pokemon = list(map(str.strip, list(input_file)))
 #     pok_set = set(pokemon)
 #     print(len(pokemon) - len(pok_set))
+
+'''Блок 4.3 Методы экземпляра класса'''
+'''№ 15 Класс Knight ♞'''
+
+# class Knight:
+#     def __init__(self, horizontal, vertical, color):
+#         self.horizontal = horizontal
+#         self.vertical = vertical
+#         self.color = color
+
+#     def char_trans(self, char):
+#         char_t = {
+#             'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6
+#         }
+#         return char_t[char.lower()]
+
+#     def get_char(self):
+#         return 'N'
+
+#     def can_move(self, h, v):
+#         h = self.char_trans(h)
+#         v_dif = abs(v - self.vertical)
+#         h_dif = abs(h -self.char_trans(self.horizontal))
+#         if (v_dif == 1 and h_dif == 2) or (v_dif == 2 and h_dif == 1):
+#             return True
+#         else:
+#             return False
+#         abs()
+
+#     def move_to(self, h, v):
+#         if self.can_move(h, v):
+#             self.horizontal = h
+#             self.vertical = v
+
+#     def draw_board(self):
+#         for i in sorted(['a', 'b', 'c', 'd', 'e', 'f', 'g'], reverse=True):
+#             for j in range(8):
+#                 if self.horizontal == i and self.vertical == j:
+#                     print(self.get_char(), end='')
+#                 elif self.can_move(i, j):
+#                     print('*', end='')
+#                 else:
+#                     print('.', end='')
+#             print()
+
+# knight = Knight('c', 3, 'white')
+# print(knight.horizontal, knight.vertical)
+# print(knight.can_move('e', 5))
+# print(knight.can_move('e', 4))
+# knight.move_to('e', 4)
+# print(knight.horizontal, knight.vertical)
+
+'''4.3'''
+'''№ 2 Класс BankAccount'''
+
+
+# class BankAccount:
+#     def __init__(self, balance=0):
+#         self._balance = balance
+
+#     def get_balance(self):
+#         return self._balance
+
+#     def deposit(self, amount):
+#         self._balance += amount
+
+#     def withdraw(self, amount):
+#         if amount > self._balance:
+#             raise ValueError('На счете недостаточно средств')
+#         self._balance -= amount
+
+#     def transfer(self, account, amount):
+#         self.withdraw(amount)
+#         account.deposit(amount)
+
+# account1 = BankAccount(100)
+# account2 = BankAccount(200)
+# try:
+#     account1.transfer(account2, 150)
+# except ValueError as e:
+#     print(e)
+
+'''№4.5.2 Класс HourClock'''
+
+
+# class HourClock:
+#     def __init__(self, hours) -> None:
+#         self.hours = hours
+
+#     def get_hours(self):
+#         return self._hours
+
+#     def set_hours(self, hours):
+#         if isinstance(hours, int) and 0 < hours < 13:
+#             self._hours = hours
+#         else:
+#             raise ValueError('Некорректное время')
+#     hours = property(get_hours, set_hours)
+
+# time = HourClock('Pizza time')
+# try:
+#     time.hours = 15
+# except ValueError as e:
+#     print(e)
--- course_OOP/mod_4/mod_4.py
+++ course_OOP/mod_4/mod_4.py
@@ -1,7 +1,93 @@
-class Cat:
-    pass
-cat = Cat()
-cat.breed = 'British'
-cat.name = 'xyz'
-cat.age = 1
-print(cat.h)
+'''4.6.1 Класс Person'''
+
+# class Person:
+#     def __init__(self, name, surname):
+#         self.name = name
+#         self.surname = surname
+
+#     @property
+#     def fullname(self):
+#         return ' '.join([self.name, self.surname])
+
+#     @fullname.setter
+#     def fullname(self, new_name=None, new_surname=None):
+#         if new_name:
+#             self.name = new_name
+#         if new_surname:
+#             self.surname = new_surname
+
+# person = Person('Mike', 'Pondsmith')
+# print(person.name)
+# print(person.surname)
+# print(person.fullname)
+
+'''4.6.3 Класс QuadraticPolynomial'''
+
+# from math import sqrt
+
+# class QuadraticPolynomial:
+#     def __init__(self, a, b, c):
+#         self.a = a
+#         self.b = b
+#         self.c = c
+
+#     @property
+#     def x1(self):
+#         a, b, c = self.a, self.b, self.c
+#         d = b ** 2 - 4 * a * c
+#         if d:
+#             return (-b - sqrt(d)) / 2 * a
+#     @property
+#     def x2(self):
+#         a, b, c = self.a, self.b, self.c
+#         d = b ** 2 - 4 * a * c
+#         if d:
+#             return (-b + sqrt(d)) / 2 * a
+
+#     @property
+#     def view(self):
+#         a, b, c = self.a, self.b, self.c
+#         return f'{a}x^2 + {b}x + {c}'
+
+#     @property
+#     def coefficients(self):
+#         return (self.a, self.b, self.c)
+
+#     @coefficients.setter
+#     def coefficients(self, a=None, b=None, c=None):
+#         if a:
+#             self.a = a
+#         if b:
+#             self.b = b
+#         if c:
+#             self.c = c
+
+# polynom = QuadraticPolynomial(1, 2, -3)
+# print(polynom.view)
+# print(polynom.coefficients)
+
+'''4.7.4 Класс Pet'''
+
+# class Pet:
+#     pets = []
+#     def __init__(self, name):
+#         self.name = name
+#         Pet.pets.append(self)
+
+#     @classmethod
+#     def first_pet(cls):
+#         return cls.pets[0] if cls.pets[0] else None
+#     @classmethod
+#     def last_pet(cls):
+#         return cls.pets[-1] if cls.pets[-1] else None
+
+#     @classmethod
+#     def num_of_pets(cls):
+#         return len(cls.pets)
+
+# pet1 = Pet('Ratchet')
+# pet2 = Pet('Clank')
+# pet3 = Pet('Rivet')
+# print(Pet.first_pet().name)
+# print(Pet.last_pet().name)
+# print(Pet.num_of_pets())
