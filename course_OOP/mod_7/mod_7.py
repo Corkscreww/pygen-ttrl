75e9300d6fff62986ed9737cf0b7cc3f8c75be00
shashkow <shashkow@gmail.com>
2024-10-02 06:34:04 +0300

    Создан каталог под новый модуль 7 и начато решение задач

---
+++ course_OOP/mod_7/mod_7.py
@@ -0,0 +1,25 @@
+'''МОДУЛЬ 7. НАСЛЕДОВАНИЕ И ПОЛИМОРФИЗМ'''
+
+'''7.1 Наследование'''
+
+'''№ 7.1.4 Классы User и PremiumUser'''
+
+# class User(object):
+#     def __init__(self, name):
+#         self.name = name
+
+#     def skip_ads(self):
+#         return False
+
+
+# class PremiumUser(User):
+#     def __init__(self, name):
+#         self.name = name
+
+#     def skip_ads(self):
+#         return True
+
+# user = User('Arthur')
+# premium_user = PremiumUser('Arthur')
+# print(user.skip_ads())
+# print(premium_user.skip_ads())
