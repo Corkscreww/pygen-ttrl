8e99832d07ae0ca994af242da3609b6905fdd648
shashkow <shashkow@gmail.com>
2024-10-22 06:42:52 +0300

    Решение задач модуль 8. Создан модуль 9

--- course_OOP/mod_8/mod_8.py
+++ course_OOP/mod_8/mod_8.py
@@ -15,9 +15,36 @@
 
 #     def __eq__(self, other):
 #         return self.area == other.area
-    
+
 #     def __lt__(self, other):
 #         return self.area < other.area
 
 # print(Shape('rectangle', 'green', 12) == Shape('triangle', 'red', 12))
 # print(Shape('triangle', 'red', 15) > Shape('triangle', 'red', 12))
+
+"""№ 8.6.2 Класс MusicAlbum"""
+
+# from dataclasses import dataclass
+
+# @dataclass(frozen=True)
+# class MusicAlbum:
+#     title: str
+#     artist: str
+#     genre: str
+#     year: int
+
+#     def __repr__(self):
+#         return f"MusicAlbum(title='{self.title}', artist='{self.artist}')"
+
+#     def __eq__(self, other):
+#         return (
+#             self.title == other.title and
+#             self.artist == other.artist and
+#             self.year == other.year
+#         )
+
+# musicalbum = MusicAlbum('Calendar', 'Motorama', 'Post-punk', 2012)
+# try:
+#     musicalbum.genre = 'Post-punk, New Wave, Indie Rock'
+# except:
+#     print('Error')
---
+++ course_OOP/mod_9/mod_9.py
@@ -0,0 +1,28 @@
+"""Модуль 9. Задачи на проектирование классов """
+
+"""№ 9.1 Функция anything()"""
+
+# class AlwaysEqual:
+#     def __eq__(self, other):
+#         return True
+#     def __ne__(self, other):
+#         return True
+#     def __lt__(self, other):
+#         return True
+#     def __gt__(self, other):
+#         return True
+#     def __le__(self, other):
+#         return True
+#     def __ge__(self, other):
+#         return True
+
+# def anything():
+#     return AlwaysEqual()
+
+# import math, re
+# print(anything() != [])
+# print(anything() < 'World')
+# print(anything() > 81)
+# print(anything() >= re)
+# print(anything() <= math)
+# print(anything() == ord)
