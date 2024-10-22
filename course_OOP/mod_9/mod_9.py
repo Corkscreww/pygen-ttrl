1f31f8f109a1a2efef8c0168ef1938971cc9dc0c
shashkow <shashkow@gmail.com>
2024-10-22 22:17:11 +0300

    Решение задач модуль 9

--- course_OOP/mod_9/mod_9.py
+++ course_OOP/mod_9/mod_9.py
@@ -26,3 +26,146 @@
 # print(anything() >= re)
 # print(anything() <= math)
 # print(anything() == ord)
+
+"""№ 9.2 Класс Vector"""
+
+# from math import sqrt
+
+# class Vector:
+#     def __init__(self, x, y, z, l=0):
+#         self.x, self.y, self.z, self.l = x, y, z, l
+
+#     def __str__(self):
+#         return f'({self.x}, {self.y}, {self.z}, {self.l})'
+
+#     def __add__(self, other):
+#         if self.l != other.l:
+#             raise ValueError('Векторы должны иметь равную длину')
+#         if isinstance(other, Vector):
+#             return Vector(
+#                 self.x + other.x,
+#                 self.y + other.y,
+#                 self.z + other.z,
+#                 self.l + other.l
+#             )
+#         return NotImplemented
+
+#     def __sub__(self, other):
+#         if self.l != other.l:
+#             raise ValueError('Векторы должны иметь равную длину')
+#         if isinstance(other, Vector):
+#             return Vector(
+#                 self.x - other.x,
+#                 self.y - other.y,
+#                 self.z - other.z,
+#                 self.l - other.l
+#             )
+#         return NotImplemented
+
+#     def __mul__(self, other):
+#         if self.l != other.l:
+#             raise ValueError('Векторы должны иметь равную длину')
+#         if isinstance(other, Vector):
+#             return Vector(
+#                 self.x * other.x,
+#                 self.y * other.y,
+#                 self.z * other.z,
+#                 self.l * other.l
+#             )
+#         return NotImplemented
+
+#     def norm(self):
+#         return sqrt(
+#             self.x ** 2 +
+#             self.y ** 2 +
+#             self.z ** 2 +
+#             self.l ** 2
+#         )
+
+# vector1 = Vector(1, 2, 3)
+# vector2 = Vector(3, 4, 5)
+# vector3 = Vector(5, 6, 7, 8)
+
+# print(vector1 + vector2)
+# print(vector1 - vector2)
+# print(vector1 * vector2)
+# print(vector3.norm())
+
+"""№ 9.3 Класс CaesarCipher"""
+
+# class CaesarCipher:
+#     def __init__(self, shift):
+#         self.shift = shift
+
+#     def encode(self, word):
+#         result = []
+#         for let in word:
+#             if 65 <= ord(let) <= 90:
+#                 code = ord(let) + self.shift
+#                 if code > 90:
+#                     new_let = chr(64 + (code - 90))
+#                 else:
+#                     new_let = chr(code)
+
+#             elif 97 <= ord(let) <= 122:
+#                 code = ord(let) + self.shift
+#                 if code > 122:
+#                     new_let = chr(96 + (code - 122))
+#                 else:
+#                     new_let = chr(code)
+
+#             else:
+#                 new_let = let
+
+#             result.append(new_let)
+#         return ''.join(result)
+
+#     def decode(self, word):
+#         result = []
+#         for let in word:
+#             if 65 <= ord(let) <= 90:
+#                 code = ord(let) - self.shift
+#                 if code < 65:
+#                     new_let = chr(91 - (65 - code))
+#                 else:
+#                     new_let = chr(code)
+
+#             elif 97 <= ord(let) <= 122:
+#                 code = ord(let) - self.shift
+#                 if code < 97:
+#                     new_let = chr(123 - (98 - code))
+#                 else:
+#                     new_let = chr(code)
+
+#             else:
+#                 new_let = let
+
+#             result.append(new_let)
+#         return ''.join(result)
+
+# cipher = CaesarCipher(5)
+# print(cipher.encode('Beegeek'))
+# print(cipher.decode('Gjjljjp'))
+
+# print(cipher.encode('Биgeek123'))
+# print(cipher.decode('Биljjp123'))
+
+"""№ 9.4 Классы ArithmeticProgression и GeometricProgression"""
+
+class ArithmeticProgression:
+    def __init__(self, fst, step):
+        self.fst, self.step = fst, step
+        self.val = self.fst - self.step
+
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        self.val += self.step
+        yield self.val
+
+progression = ArithmeticProgression(0, 1)
+for elem in progression:
+    if elem > 10:
+        break
+    print(elem, end=' ')
